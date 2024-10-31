f9842f8675da30988cdd82096a11ee155c2ad95e
Juan Loretto <juanloretto29@gmail.com>
2024-10-31 18:40:14 -0300

    verifico conexion a la bd

--- database/config.js
+++ database/config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const dbConnection = async ()=>{
 try {
+    console.log('Intentando conectar a la base de datos:', process.env.DATABASE_CNN);
     await mongoose.connect(process.env.DATABASE_CNN)
     console.log('Base de datos online')
 } catch ( error) {
